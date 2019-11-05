18025f3c2467746aa7938ba8804b5510c14bbed3
lambui09 <buiduclam09@gmail.com>
2019-11-05 17:38:24 +0700

    init camera x

--- CameraX/app/src/main/java/com/example/cameraxdemo/MainActivity.kt
+++ CameraX/app/src/main/java/com/example/cameraxdemo/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.cameraxdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.TextureView
+import androidx.lifecycle.LifecycleOwner
+import java.lang.reflect.Executable
+import java.util.concurrent.Executors
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LifecycleOwner {
+    private val executor = Executors.newSingleThreadExecutor()
+    private var viewFinder : TextureView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    private fun startCamera(){
+
+    }
+
+    private fun updateTransform(){
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        
     }
 }
